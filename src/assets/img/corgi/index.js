1893046a10ae8d149a436300c4d66aa6aeb96fd3
8Lou <lou8@mail.ru>
2023-11-10 15:22:12 +0300

    style

--- src/assets/img/corgi/index.js
+++ src/assets/img/corgi/index.js
@@ -1,13 +1,13 @@
-import pic1 from "./corgi1.png"
-import pic2 from "./corgi2.png"
-import pic3 from "./corgi3.png"
-import pic4 from "./corgi4.png"
-import pic5 from "./corgi5.png"
-import pic6 from "./corgi6.png"
-import pic7 from "./corgi7.png"
-import pic8 from "./corgi8.png"
-import pic9 from "./corgi9.png"
-import pic10 from "./corgi10.png"
+import pic1 from "./corgi2.png"
+import pic2 from "./corgi4.png"
+import pic3 from "./corgi4.png"
+import pic4 from "./corgi1.png"
+import pic5 from "./corgi1.png"
+import pic6 from "./corgi1.png"
+import pic7 from "./corgi1.png"
+import pic8 from "./corgi1.png"
+import pic9 from "./corgi1.png"
+import pic10 from "./corgi1.png"
 
 const Corgi = {
     toys_ball: pic1,
